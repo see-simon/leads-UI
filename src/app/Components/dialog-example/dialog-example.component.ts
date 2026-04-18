91abd1e816b7776e51de35f1fe40bce8267f3fdf
mpelemane <sea6580@gmail.com>
2026-04-18 20:40:08 +0200

    final validations

--- src/app/Components/dialog-example/dialog-example.component.ts
+++ src/app/Components/dialog-example/dialog-example.component.ts
@@ -13,36 +13,48 @@ export class DialogExampleComponent {
   userForm: FormGroup;
 
   constructor(private fb: FormBuilder, private leadsService: LeadsService, private dialogRef: MatDialogRef<DialogExampleComponent>,
-    @Inject(MAT_DIALOG_DATA) public stateData: any ,   private router: Router, // ✅ ADD THIS
+    @Inject(MAT_DIALOG_DATA) public stateData: any, private router: Router, // ✅ ADD THIS
   ) {
 
     this.userForm = this.fb.group({
-      name: ['', Validators.required],
-      surname: ['', Validators.required],
+      name: ['', [Validators.required, Validators.pattern('^[a-zA-Z ]+$')]],
+      surname: ['', [Validators.required, Validators.pattern('^[a-zA-Z ]+$')]],
       email: ['', [Validators.required, Validators.email]],
-      phoneNumber: ['', Validators.required],
-      
-     description: [`${this.stateData.title}`, Validators.required]
+      phoneNumber: ['', [Validators.required, Validators.pattern('^0[0-9]{9}$')]],
+
+      description: [`${this.stateData.title}`, Validators.required]
     });
   }
 
-
-onSubmit() {
-  if (this.userForm.valid) {
-
-    this.leadsService.addLeads(this.userForm.value).subscribe(response => {
-      console.log('Lead added successfully', response);
-
-      // ✅ Close dialog first
-      this.dialogRef.close();
-      alert('Thank you! A consultant will contact you shortly.');
-      // ✅ Then navigate to landing page
-      this.router.navigate(['/landing-page']);
-
-    }, error => {
-      console.error('Error adding lead', error);
-    });
-
+  onlyText(event: KeyboardEvent) {
+    const char = String.fromCharCode(event.keyCode);
+    if (!/^[a-zA-Z ]+$/.test(char)) {
+      event.preventDefault();
+    }
   }
-}
+
+  onlyNumbers(event: KeyboardEvent) {
+    const char = String.fromCharCode(event.keyCode);
+    if (!/^[0-9]+$/.test(char)) {
+      event.preventDefault();
+    }
+  }
+  onSubmit() {
+    if (this.userForm.valid) {
+
+      this.leadsService.addLeads(this.userForm.value).subscribe(response => {
+        console.log('Lead added successfully', response);
+
+        // ✅ Close dialog first
+        this.dialogRef.close();
+        alert('Thank you! A consultant will contact you shortly.');
+        // ✅ Then navigate to landing page
+        this.router.navigate(['/landing-page']);
+
+      }, error => {
+        console.error('Error adding lead', error);
+      });
+
+    }
+  }
 }
