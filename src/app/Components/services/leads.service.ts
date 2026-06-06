8163cd092a6c357d274df6cde2475210d4878617
see-simon <sea6580@gmail.com>
2026-06-06 18:21:56 +0200

    before hosting

--- src/app/Components/services/leads.service.ts
+++ src/app/Components/services/leads.service.ts
@@ -6,11 +6,15 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class LeadsService {
-  private baseUrl = 'http://localhost:8080/api/leads';
+  private baseUrl = 'http://localhost:5053/api/leads';
 
-  constructor( private http: HttpClient ) { }
+  constructor(private http: HttpClient) {}
 
-  addLeads(lead: any) :Observable<any> {
-    return this.http.post(this.baseUrl, lead,{responseType: 'json'});
+  addLeads(lead: any): Observable<any> {
+    return this.http.post(this.baseUrl, lead, { responseType: 'json' });
+  }
+
+  getLeads(): Observable<any> {
+    return this.http.get(this.baseUrl);
   }
 }
